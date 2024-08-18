9886604330c85b0526ff310e3b07207abc484519
Giancarlo Bastos Martins <gbastos-@c2r7p5.42sp.org.br>
2024-08-18 17:14:25 -0300

    rush02.c

--- rush02.c
+++ rush02.c
@@ -6,40 +6,60 @@
 /*   By: gbastos- <gbastos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 21:46:26 by gbastos-          #+#    #+#             */
-/*   Updated: 2024/08/17 23:12:01 by gbastos-         ###   ########.fr       */
+/*   Updated: 2024/08/18 16:37:21 by gbastos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char character)
 {
-	write(1, &c, 1);
+	write(1, &character, 1);
 }
 
 void	rush(int x, int y)
 {
-	int	row;
-	int	col;
+	int	i;
+	int	j;
 
-	if (x <= 0 || y <= 0)
-		return ;
-	row = 0;
-	while (row < y)
+	i = 0;
+	while (i < x)
 	{
-		col = 0;
-		while (col < x)
+		j = 0;
+		while (j < y)
 		{
-			if ((row == 0 && col == 0) || (row == 0 && col == x - 1)
-				|| (row == y - 1 && col == 0) || (row == y - 1 && col == x - 1))
-				ft_putchar('A');
-			else if (row == 0 || row == y - 1 || col == 0 || col == x - 1)
-				ft_putchar('B');
-			else
-				ft_putchar(' ');
-			col++
+			if (i == 0)
+			{
+				if (j == 0 || j == y - 1)
+				{
+					ft_putchar('A');
+				}
+				else
+					ft_putchar('B');
+			}
+			else if (i == x - 1)
+			{
+				if (j == 0 || j == y - 1)
+				{
+					ft_putchar('C');
+				}
+				else
+					ft_putchar('B');
+			}
+			else if (i != 0 || i != x-1){
+				if (j == 0 || j == y -1)
+					ft_putchar('B');
+				else
+					ft_putchar(' ');
+			}
+			j++;
 		}
 		ft_putchar('\n');
-		row++;
+		i++;
 	}
+}
+int	main(void)
+{
+	rush(123,47);
+	return (0);
 }
