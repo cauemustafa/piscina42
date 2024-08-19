455fc08f60cc6442776bdc6c5f5ee2f5f4c0755e
Caue Mustafa Assem <caue@42musta.tech>
2024-08-18 21:02:17 -0300

    feat(main): calling rush function with parameters for test the output like the pdf

--- ex00/main.c
+++ ex00/main.c
@@ -3,17 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbastos- <gbastos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cassem <cassem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 15:58:02 by gbastos-          #+#    #+#             */
-/*   Updated: 2024/08/18 17:12:37 by gbastos-         ###   ########.fr       */
+/*   Updated: 2024/08/18 19:47:54 by cassem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	rush(int x, int y);
+void	ft_putchar(char character);
 
 int	main(void)
 {
 	rush(5, 3);
+	ft_putchar('\n');
+	rush(5, 1);
+	ft_putchar('\n');
+	rush(1, 1);
+	ft_putchar('\n');
+	rush(4, 4);
 	return (0);
 }
