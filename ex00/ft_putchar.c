13f0146dccc9ca689dbeb68fe087f37795e5eadb
Caue Mustafa Assem <caue@42musta.tech>
2024-08-21 19:24:21 -0300

    refactor(c00/ex00): format code with norminette

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,13 +6,14 @@
 /*   By: cassem <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 18:05:16 by cassem            #+#    #+#             */
-/*   Updated: 2024/08/16 18:40:01 by cassem           ###   ########.fr       */
+/*   Updated: 2024/08/21 19:22:26 by cassem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c){
+void	ft_putchar(char c)
+{
 	write(1, &c, 1);
 }
 
