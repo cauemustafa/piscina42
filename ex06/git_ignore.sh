e7959e6b9bc8fc464eda2c13b9bb5b4d820896d3
cauemustafa <caue@42musta.tech>
2024-08-23 10:31:54 -0300

    build: remove directories of shell00/ex00 of the branch main

--- ex06/git_ignore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git ls-files --ignored --others --exclude-standard
-
-# git ls-files: Lista os arquivos do repositório.
-# --ignored: Mostra arquivos que são ignorados com base nas regras definidas em .gitignore, .git/info/exclude, e outras configurações de exclusão.
-# --others: Inclui arquivos que não estão sendo rastreados pelo Git.
-# --exclude-standard: Aplica as regras de exclusão padrão do Git, garantindo que o comando considere as regras definidas em .gitignore, .git/info/exclude, e core.excludesFile.
