bb19d418e5f857b9b7d1915903a0daf62d5ea671
Caue Mustafa Assem <caue@42musta.tech>
2024-08-30 22:12:51 -0300

    feat(c01/ex00): forty two - forty two

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -6,9 +6,11 @@
 /*   By: cassem <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 11:22:31 by cassem            #+#    #+#             */
-/*   Updated: 2024/08/30 19:45:57 by cassem           ###   ########.fr       */
+/*   Updated: 2024/08/30 22:08:43 by cassem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	ft_ft(int *nbr)
 {
@@ -16,17 +18,17 @@ void	ft_ft(int *nbr)
 }
 
 /*
-#include <stdio.h>
-int main()
+int	main(void)
 {
-	int n;
-	int *nbr;
+	int	n;
+	int	*nbr;
 
 	n = 0;
 	nbr = &n;
-	printf("Before ft_ft: n = %d\n", n);
+	printf("nbr = &n\nn = 0\n");
+	printf("Value pointed to by nbr: %d\n", *nbr);
 	ft_ft(nbr);
-	printf("After ft_ft: n = %d\n", n);
+	printf("After ft_ft(nbr): n = %d\n", n);
 	return (0);
 }
 */
