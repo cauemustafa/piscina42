f593f9827878c7d54774dea608293cbf75bf5d58
Caue Mustafa Assem <caue@42musta.tech>
2024-09-02 20:05:16 -0300

    feat(c01/ex01): create a function to change the value to 42 of the pointer of the pointer of the pointer...

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassem <cassem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 22:18:29 by cassem            #+#    #+#             */
+/*   Updated: 2024/09/02 20:00:12 by cassem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr){
+	*********nbr = 42;
+}
+
+/* int main(void){
+	int n;
+	int *ptr0;
+	int **ptr1;
+	int ***ptr2;
+	int ****ptr3;
+	int *****ptr4;
+	int ******ptr5;
+	int *******ptr6;
+	int ********ptr7;
+	int *********nbr;
+	
+	n = 0;
+	ptr0 = &n;
+	ptr1 = &ptr0;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	nbr = &ptr7;
+	printf("Before: %d\n", n);
+	ft_ultimate_ft(nbr);
+	printf("After: %d", n);
+	return 0;
+} */
