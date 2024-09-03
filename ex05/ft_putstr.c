5a630eedd9d5db586738c9696a94c555d3a5cbf0
Caue Mustafa Assem <caue@42musta.tech>
2024-09-02 21:56:32 -0300

    feat(c01/ex05): add ft_putstr function
    - Implemented ft_putstr to print a string using write.

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassem <cassem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 20:38:39 by cassem            #+#    #+#             */
+/*   Updated: 2024/09/02 20:51:50 by cassem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+/* int	main(void)
+{
+	char	*str;
+
+	str = "Hello, World!";
+	ft_putstr(str);
+	return (0);
+}
+ */
