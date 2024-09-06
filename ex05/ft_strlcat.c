59d63deb25a36340c7839904084653e8bb9faa94
Caue Mustafa Assem <caue@42musta.tech>
2024-09-05 23:35:08 -0300

    feat(ex05): implement ft_strlcat function
    - Added ft_strlcat function to concatenate strings
    - Calculates the length of the destination and source strings
    - Appends the source string to the destination string
    - Ensures null termination of the destination string
    - Returns the total length of the resulting string
    - Handles the case where the destination buffer is too small
    - Uses ft_strlen function to calculate string lengths

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassem <cassem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 22:05:59 by cassem            #+#    #+#             */
+/*   Updated: 2024/09/05 23:30:51 by cassem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size <= dest_len)
+		return (src_len + size);
+	while (src[i] && (dest_len + i) < (size - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
+/* 
+int main()
+{
+	char dest[20] = "Hello";
+	char src[] = "World";
+	unsigned int size = sizeof(dest);
+	unsigned int result = ft_strlcat(dest, src, size);
+	printf("Result: %s\nLength: %u\n", dest, result);
+	return 0;
+}
+*/
