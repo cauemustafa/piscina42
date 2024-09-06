76afab87437838dca73aa3481dca1cb3546fe2e7
Caue Mustafa Assem <caue@42musta.tech>
2024-09-06 01:37:26 -0300

    refactor(ex00): last try, use iterator instead of pointers

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: cassem <cassem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 21:16:29 by cassem            #+#    #+#             */
-/*   Updated: 2024/09/05 19:59:57 by cassem           ###   ########.fr       */
+/*   Updated: 2024/09/06 00:52:48 by cassem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	while (*src)
-		*dest++ = *src++;
-	*dest = '\0';
+	int	i;
+
+	i = -1;
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = '\0';
 	return (dest);
 }
-/* 
-int	main(void)
-{
-	char	*src;
-	char	dest[20];
-	char *address;
 
-	src = "Hello, World!";
-	address = ft_strcpy(dest, src);
+/* 
+int main(void)
+{
+	char *src = "Hello, World!";
+	char dest[100];
+	char *test;
+
+	test = ft_strcpy(dest, src);
+
 	printf("Source: %s\n", src);
 	printf("Destination: %s\n", dest);
-	printf("Address: %p\n", address);
-	return (0);
-}
- */
+	printf("test: %p\n", test);
+
+	return 0;
+} */
